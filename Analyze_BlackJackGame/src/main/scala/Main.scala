2c44ccf92d398c497d7c6205c33997609a5aa33e
AsadaGuitar <79627592+AsadaGuitar@users.noreply.github.com>
2021-07-13 22:58:05 +0900

    追加
    関数のリファクタリング

--- Analyze_BlackJackGame/src/main/scala/Main.scala
+++ Analyze_BlackJackGame/src/main/scala/Main.scala
@@ -2,76 +2,117 @@ import Tramp._
 
 object Main extends App {
 
+  //ブラックジャックの最大点
   val BLACK_JACK: Int = 21
+  //ディーラーが引くのを止める点
   val DEALERS_BORDER: Int = 16
+  //ヒットする際判断の材料となるパーセンテージ
   val HIT_PERCENT: Double = 0.5
-  val PLAYING_TRAMPS = Seq(Ace,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Eleven,Twelve,Thirteen)
+  //ゲームで使うトランプ
+  val PLAYING_TRAMPS = Seq(Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Eleven, Twelve, Thirteen)
 
-  def isOver(trampNum: Int, border: Int): Boolean = border < trampNum
+  //数字からトランプを取得
+  val tramp: (Int,Seq[Tramp]) => Option[Tramp] =
+    (num: Int, tramp: Seq[Tramp]) => tramp.find(_.num == num)
 
-  def initDeck(num: Int, tramps: Seq[AbsTramp]): Map[AbsTramp,Int] ={
-    val deck = tramps.foldLeft(Map[AbsTramp, Int]()) {(m, s) => m ++ Map(s -> 4)}
-    if(num == 1) return deck
-    deck.map(x => (x._1, x._2 * num))
+  //Aceを考慮したユーザーの手札を返却
+  val handScore: (Tramp, Tramp) => Seq[Int] =
+    (x: Tramp, y: Tramp) =>
+      if(x == Ace || y == Ace) {
+        if (x == Ace && y == Ace)
+          Seq(12, 2)
+        else Seq(x.num + y.num + 10)
+      }
+      else Seq(x.num + y.num)
+
+
+  //山札の初期化
+  def initDeck(deckNum: Int, tramps: Seq[Tramp]): Seq[Tramp] = {
+    def addTramps(num: Int): Seq[Tramp] =
+      if(num <= 1) tramps
+      else tramps ++ addTramps(num - 1)
+
+    if (deckNum < 1) Nil
+    else addTramps(deckNum *4)
   }
 
-  val getTramp: (Int,Seq[AbsTramp]) => AbsTramp
-  = (num: Int, playingTramps: Seq[AbsTramp]) => playingTramps.filter(t => t.num == num).head
+  //山札からカードを削除
+  def deleteTramp(tramp: Tramp, deck: Seq[Tramp]): Seq[Tramp] = deck.diff(Seq(tramp))
 
-  def deleteTramp(tramp: AbsTramp, deck: Map[AbsTramp,Int]): Map[AbsTramp,Int] ={
-    if(deck(tramp) == 0) throw new IllegalStateException("カード枚数が負数です。")
-    deck.updated(tramp, deck(tramp) -1)
+  //山札、もしくは全てのトランプから指定の範囲のトランプを検索
+  //range.endが11の場合、Aceを含める
+  def findTrampsByRange(range: Range, tramps: Seq[Tramp]): Seq[Tramp] ={
+    val trampsInRange: Seq[Tramp] = tramps.filter(range contains _.num)
+    if(range.end == 11) trampsInRange ++ tramps.filter(_ == Ace)
+    else trampsInRange
   }
+
+  //山札から指定のトランプを引く確率
+  val ratioTrampsRangeInDeck: (Range,Seq[Tramp]) => Float =
+    (range: Range, deck: Seq[Tramp]) => deck.count(range contains _.num) / deck.size.toFloat
+
+  //得点と、最高得点を受取り、得点と合わせた時に最高得点を超えてしまう得点を返却
+  val burstRange: (Int, Int) => Range =
+    (score: Int, blackJack: Int) =>
+      if(score >= blackJack) 1 to 11
+      else blackJack - score + 1 to 11
 
   //*******************************************************
+
 
   println("デッキの数を入力してください。")
   val numberOfDeck = io.StdIn.readInt()
   var deck = initDeck(numberOfDeck,PLAYING_TRAMPS)
 
   println("あなたの手札を教えてください。")
+
   println("一枚目 : ")
   val userHandA = io.StdIn.readInt()
   println("二枚目 : ")
   val userHandB = io.StdIn.readInt()
-
   println("ディーラーの手札を教えてください。")
   val dealerHand = io.StdIn.readInt()
 
-  deck = deleteTramp(getTramp(userHandA,PLAYING_TRAMPS),deck)
-  deck = deleteTramp(getTramp(userHandB,PLAYING_TRAMPS),deck)
-  deck = deleteTramp(getTramp(dealerHand,PLAYING_TRAMPS),deck)
+  //各々が引いたカードを山札から削除
+  deck = deleteTramp(tramp(userHandA,PLAYING_TRAMPS).get, deck)
+  deck = deleteTramp(tramp(userHandB,PLAYING_TRAMPS).get, deck)
+  deck = deleteTramp(tramp(dealerHand,PLAYING_TRAMPS).get, deck)
 
-  println("deck : " + deck)
+  println(deck)
 
-  //手札の合計
-  val userTotalNum = userHandA + userHandB
-  println("userTotalNum : " + userTotalNum)
+  println(1 - ratioTrampsRangeInDeck(burstRange(userHandA+userHandB, BLACK_JACK), deck))
 
-  //手札と足して21を超えない数値を代入
-  val cardsRangeAblePull: Range = 1 to BLACK_JACK - userTotalNum
-  println("cardsRangeAblePull : " + cardsRangeAblePull)
+//  //手札の合計
+//  val userTotal = userHandA + userHandB
+//  println("userTotalNum : " + userTotal)
+//
+//  //バーストしないカード
+//  println("ablePullCards : " + ablePullCards(userTotal,deck))
+//  //バーストしないカードを引ける確率
+//  println("rationNumbersInDeck : " + ratioNumbersInDeck(noBurstRange(userTotal),deck))
+//  //Aceが1、11の時の山札の総和
+//  println("averagePull : " + averagePull(deck))
+//
+//  //ディーラーの手札を平均点から予測
+//  val dealerTotalA = addScoreUntilBorder(dealerHand,averagePull(deck)._1,DEALERS_BORDER)
+//  val dealerTotalB = addScoreUntilBorder(dealerHand,averagePull(deck)._2,DEALERS_BORDER)
+//  println("dealerTotalA : " + dealerTotalA)
+//  println("deaderTotalB : " + dealerTotalB)
+//
+//  //ディーラーの手札が自分の手札よりも強い確率
+//  val r = BLACK_JACK - (userTotal - dealerHand)
 
-  //山札に存在するカードの中で、手札と足して21を超えないカードの数値を代入
-  val cardsRangeAblePullInTramp: Range = {
-    if(cardsRangeAblePull.end > 10) 1 to 10
-    else cardsRangeAblePull
-  }
-  println("cardsRangeAblePullInTramp : " + cardsRangeAblePullInTramp)
+  //ユーザーがヒットした場合の手札の合計点
+//  val nextUserTotal = userTotalNum + averageAblePull
 
-  //手札と足して21を超えない全てのカードを代入
-  val ablePullCardsMapSeq: Seq[Map[AbsTramp,Int]] = for(c <- cardsRangeAblePullInTramp) yield deck.filter(_._1.num == c)
-  println("ablePullCardsMapSeq : " + ablePullCardsMapSeq)
+  //ディーラーの手札の平均点
+//  val dealerTotalHandsAverage = averageAblePullCardsPoints + dealerHand
 
-  //手札と足して21を超えない全てのカードの残り枚数を代入
-  val ablePullCardsCounts: Seq[Int] = for(m <- ablePullCardsMapSeq) yield m.foldLeft(0)((acc,x) => acc + x._2)
-  println("ablePullCardsCounts : " + ablePullCardsCounts)
-
-  //山札の中で、手札と足して21を超えないカードの総和の割合
-  println("ablePullCardsCounts.sum : " + ablePullCardsCounts.sum)
-  println("deck.values.sum : " + deck.values.sum)
-  val ratioAblePullCards: Double = ablePullCardsCounts.sum / deck.values.sum.toFloat
-  println("ratioAblePullCards : " + ratioAblePullCards)
+//  if(userTotalNum < dealerTotalHandsAverage && ratioAblePullCards > HIT_PERCENT && isBurst(nextUserTotal)){
+//    println("HIT")
+//  }
+//  else if(userTotalNum < delerTotalHandsAverage && raionAblePullCards < HIT_PERCENT){ //ここら辺修正
+//    println("Surrender")
+//  }
+//  else println("Stand")
 }
-
-
