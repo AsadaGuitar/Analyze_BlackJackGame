3fa6c7cbc645649e0002bb7fe27f0ada6cc8ac0d
JavaUser <79627592+AsadaGuitar@users.noreply.github.com>
2021-09-03 00:40:51 +0900

    overrideの削除

--- Analyze_BlackJack/src/main/scala/com/analysis/common/calculation/ProbsMaker.scala
+++ Analyze_BlackJack/src/main/scala/com/analysis/common/calculation/ProbsMaker.scala
@@ -3,7 +3,7 @@ package com.analysis.common.calculation
 import com.analysis.common.calculation._
 
 implicit class ProbsMaker[I <: Iterable[(K, Rational)], K](l: I){
-  override def toProbs: Probs[K] = {
+  def toProbs: Probs[K] = {
     l.foldLeft(Nil: Probs[K])((acc,x) => acc.addValue(x))
   }
 }
