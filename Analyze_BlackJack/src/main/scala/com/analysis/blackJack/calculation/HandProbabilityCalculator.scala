5799af1012c33eb6f45cc91c6b645d49287f0953
JavaUser <79627592+AsadaGuitar@users.noreply.github.com>
2021-09-03 00:42:40 +0900

    コンパニオンオブジェクトの削除

--- Analyze_BlackJack/src/main/scala/com/analysis/blackJack/calculation/HandProbabilityCalculator.scala
+++ Analyze_BlackJack/src/main/scala/com/analysis/blackJack/calculation/HandProbabilityCalculator.scala
@@ -4,20 +4,12 @@ import com.analysis.blackJack.{Deck, Hand}
 import com.analysis.common.*
 import com.analysis.common.calculation.Rational
 
-/*
-オブジェクト名　  HandProbabilityCalculator
-機能            HandProbabilityCalculatorクラスのコンパニオンオブジェクト
-*/
-object HandProbabilityCalculator {
-  
-}
 
 /*
 トレイト名   HandProbabilityCalculator
 機能        引く可能性のある手札の確率を取得
 */
 trait HandProbabilityCalculator {
-  import HandProbabilityCalculator._
 
   /*
   メソッド名     calculateProb
