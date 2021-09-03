ca1280e1a01b9f5186cc8087afc31d1cf70e6e73
JavaUser <79627592+AsadaGuitar@users.noreply.github.com>
2021-09-03 17:33:02 +0900

    コメントの修正

--- Analyze_BlackJack/src/main/scala/com/analysis/common/io/MonadicIO.scala
+++ Analyze_BlackJack/src/main/scala/com/analysis/common/io/MonadicIO.scala
@@ -7,8 +7,8 @@ object MonadicIO {
   /*
   メソッド名     putStrLn
   機能          「文字列を標準出力する動作」を返却
-  引数          str: String     出力する文字列
-  戻値          IO[Unit]        「出力する動作」を返却
+  引数          str: String     　出力する文字列
+  戻値          IO[Unit]        　「出力する動作」を返却
   */
   def putStrLn(str: String): IO[Unit] = IO(println(str))
 
@@ -21,11 +21,8 @@ object MonadicIO {
 
   /*
   メソッド名     readInt
-  機能          「メッセージを出力し、数値を読取るまで入力を繰り返す動作」を返却
-  引数          msg: String                 出力する文字列
-  　　          errMsg: String              例外メッセージ
-  　　          filter: String => Boolean   読取る数値のフィルター
-  戻値          IO[Unit]                    「メッセージを出力し、数値を読取るまで入力を繰り返す動作」
+  機能          「文字列を読込み、数値変換した結果を返却する動作」を返却
+  戻値          IO[Unit]           「メッセージを出力し、数値を読取るまで入力を繰り返す動作」
   */
   def readInt(): IO[Either[NumberFormatException, Int]] = {
     try {
