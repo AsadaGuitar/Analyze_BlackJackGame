226a29c3b4935ddc808524181d231845ed5fa718
JavaUser <79627592+AsadaGuitar@users.noreply.github.com>
2021-09-03 17:45:35 +0900

    コメントの追加

--- Analyze_BlackJack/src/main/scala/com/analysis/common/calculation/ProbsMaker.scala
+++ Analyze_BlackJack/src/main/scala/com/analysis/common/calculation/ProbsMaker.scala
@@ -2,7 +2,18 @@ package com.analysis.common.calculation
 
 import com.analysis.common.calculation._
 
+/*
+クラス名      ProbsMaker[I <: Iterable[(K, Rational)], K]
+機能　　      Iterableを上限境界としたオブジェクトをProbsに変換
+引数          l: I
+*/
 implicit class ProbsMaker[I <: Iterable[(K, Rational)], K](l: I){
+  
+  /*
+  メソッド名     toProbs
+  機能          Iterableを上限境界としたオブジェクトをProbsに変換
+  戻値          Probs[K]      Probsクラス
+  */
   def toProbs: Probs[K] = {
     l.foldLeft(Nil: Probs[K])((acc,x) => acc.addValue(x))
   }
