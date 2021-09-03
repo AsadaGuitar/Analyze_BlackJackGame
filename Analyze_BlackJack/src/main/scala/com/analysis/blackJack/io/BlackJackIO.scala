031b45dd8f7fa158bde6fb38ef43db88756676e7
JavaUser <79627592+AsadaGuitar@users.noreply.github.com>
2021-09-03 18:27:30 +0900

    戻値をIO[?]からIO[Either[?]]に変換

--- Analyze_BlackJack/src/main/scala/com/analysis/blackJack/io/BlackJackIO.scala
+++ Analyze_BlackJack/src/main/scala/com/analysis/blackJack/io/BlackJackIO.scala
@@ -12,36 +12,32 @@ import com.analysis.common.AnalysisIO._
 object BlackJackIO {
 
   /*
-  メソッド名     readAction
-  機能          「メッセージを出力し、アクションを標準入力で読取る動作」を返却
-  引数          msg: String       出力するメッセージ
-  　　          errMsg: String    例外メッセージ
-  戻値          IO[Action]        「メッセージを出力し、アクションを標準入力で読取る動作」
+  メソッド名       readAction
+  機能            「アクションを標準入力で読取る動作」を返却
+  戻値            IO[Either[Action]]                               「メッセージを出力し、アクションを標準入力で読取る動作」
   */
-  def readAction(): IO[Action] =
+  def readAction(): IO[Either[Action]] =
     //アクション読込
-    readLn().flatMap(x => x.toUpperCase match {
-      case "HIT" => IO(Hit) 
-      case "DOUBLEDOWN" => IO(DoubleDown) 
-      case "STAND" => IO(Stand) 
-      //不正な値を読込んだ場合、例外メッセージを出力し再度実行
-      case _ => readAction(msg, errMsg)
+    readLn().map(x => x.toUpperCase match {
+      case "HIT" => Right(Hit) 
+      case "DOUBLEDOWN" => Right(DoubleDown) 
+      case "STAND" => Right(Stand) 
+      //不正な値を読込んだ場合、Either.Leftを返却
+      case _ => Left(throw new IllegalArgumentException)
   })
   
   /*
   メソッド名     readSystemCommand
-  機能          「メッセージを出力し、システムコマンドを標準入力で読取る動作」を返却
-  引数          msg: String      出力メッセージ
-  　　          errMsg: String   例外メッセージ
-  戻値          SystemCommand    「メッセージを出力し、システムコマンドを標準入力で読取る動作」
+  機能          「システムコマンドを標準入力で読取る動作」を返却
+  戻値          IO[Either[SystemCommand]]                          「メッセージを出力し、システムコマンドを標準入力で読取る動作」
   */
-  def readSystemCommand(): IO[SystemCommand] =
+  def readSystemCommand(): IO[Either[SystemCommand]] =
     //文字列読込
-    readLn().flatMap(line => line.toUpperCase match {
-      case "CONTINUE" => IO(Continue)
-      case "INIT" => IO(Init)
-      case "FINISH" => IO(Finish)
+    readLn().map(line => line.toUpperCase match {
+      case "CONTINUE" => Right(Continue)
+      case "INIT" => Right(Init)
+      case "FINISH" => Right(Finish)
       //不正な文字列を受取った場合、例外メッセージを出力し再度実行
-      case _ => readSystemCommand()
+      case _ => Left(throw new IllegalArgumentException)
     })
 }
